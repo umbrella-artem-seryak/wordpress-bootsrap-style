743917088d8908c13e53f08fae4795a110247822
umbrella-artem-seryak <artem.seryak@umbrella-web.com>
2016-11-03 13:23:04 +0300

    Style of next-post and previous post buttons was updated

--- single.php
+++ single.php
@@ -17,7 +17,16 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_format() );
 
-			the_post_navigation();
+			the_post_navigation( array(
+				'next_text' => '<button type="button" class="btn btn-default">' .
+					'<span class="post-title">%title</span>' .
+					'<span class="glyphicon glyphicon-chevron-right"></span>' .
+					'</button>',
+				'prev_text' => '<button type="button" class="btn btn-default">' .
+					'<span class="glyphicon glyphicon-chevron-left"></span>' .
+					'<span class="post-title">%title</span>' .
+					'</button>',
+			) );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
