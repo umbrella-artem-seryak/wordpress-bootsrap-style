6d124ad3190e4441c1e9a610037ea08b5a1fdaf1
umbrella-artem-seryak <artem.seryak@umbrella-web.com>
2016-11-03 11:26:28 +0300

    Updated Nav menu to be more like a bootstrap's standard one. Code formatting in header.php updated. Added filter function in functions.php to replace standard 'current-menu-item' with bootstrap's 'active' class

--- functions.php
+++ functions.php
@@ -115,6 +115,18 @@ function bootstrap_theme_scripts() {
 add_action( 'wp_enqueue_scripts', 'bootstrap_theme_scripts' );
 
 /**
+ * Replacing active li element (for the nav menu) css class here
+ */
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+    if (in_array('current-menu-item', $classes) ){
+        $classes[] = 'active ';
+    }
+    return $classes;
+}
+
+/**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
--- header.php
+++ header.php
@@ -12,42 +12,52 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
 
-<?php wp_head();
+    <?php wp_head();
 
-wp_enqueue_style( 'slider', get_template_directory_uri() . '/bootstrap/css/bootstrap.css');
-?>
+    wp_enqueue_style('slider', get_template_directory_uri() . '/bootstrap/css/bootstrap.css');
+    ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bootstrap-theme' ); ?></a>
+    <a class="skip-link screen-reader-text"
+       href="#content"><?php esc_html_e('Skip to content', 'bootstrap-theme'); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+    <header id="masthead" class="site-header" role="banner">
+        <div class="site-branding">
+            <?php
+            if (is_front_page() && is_home()) : ?>
+                <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                          rel="home"><?php bloginfo('name'); ?></a></h1>
+            <?php else : ?>
+                <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                         rel="home"><?php bloginfo('name'); ?></a></p>
+                <?php
+            endif;
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+            $description = get_bloginfo('description', 'display');
+            if ($description || is_customize_preview()) : ?>
+                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+                <?php
+            endif; ?>
+        </div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bootstrap-theme' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-		<?php get_search_form(true); ?>
-	</header><!-- #masthead -->
+        <nav id="site-navigation" class="navbar navbar-default" role="navigation">
+            <button class="menu-toggle" aria-controls="primary-menu"
+                    aria-expanded="false"><?php esc_html_e('Primary Menu', 'bootstrap-theme'); ?></button>
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#"><?php bloginfo('name') ?> </a>
+                </div>
+                <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class' => 'nav navbar-nav')); ?>
+            </div>
 
-	<div id="content" class="site-content">
+        </nav><!-- #site-navigation -->
+        <?php get_search_form(true); ?>
+    </header><!-- #masthead -->
+
+    <div id="content" class="site-content">
