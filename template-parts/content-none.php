db2e1802d5ab6a5ad3adca52dbb88f75ae947948
umbrella-artem-seryak <artem.seryak@umbrella-web.com>
2016-11-07 13:20:12 +0300

    Search form deleted from the search results (in case nothing was actually found). No need to display it since we have the same form included in the header.

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -23,15 +23,11 @@
 		<?php elseif ( is_search() ) : ?>
 
 			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'bootstrap-theme' ); ?></p>
-			<?php
-				get_search_form();
 
-		else : ?>
+		<?php else : ?>
 
 			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'bootstrap-theme' ); ?></p>
-			<?php
-				get_search_form();
 
-		endif; ?>
+		<?php endif; ?>
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
