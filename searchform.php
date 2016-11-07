8d50068283224609d8955b1d9e9c8749e1e1b04d
umbrella-artem-seryak <artem.seryak@umbrella-web.com>
2016-11-07 13:30:19 +0300

    Search form moved to the right hand side of the screen

--- searchform.php
+++ searchform.php
@@ -4,7 +4,7 @@
  */
 ?>
 
-<form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <div class="form-group">
     <label>
         <input type="search" class="form-control" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'bootstrap-theme' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
